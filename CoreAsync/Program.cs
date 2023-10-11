fcf0bc3515d39f91726cdc9e2c3f3291fed882b4
Vivek Lakhlani <viveklakhlani.mscit20@vnsgu.ac.in>
2023-10-12 03:43:20 +0530

    Add project files.

---
+++ CoreAsync/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CoreAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            asyncdisplay("hello world using asynchornous programming");
+            asynccloseprog("Closing asynchornous program");
+            Console.ReadKey();
+        }
+        static async Task asyncdisplay(string msg)
+        {
+            Console.WriteLine("Async task display starts");
+            await Task.Delay(8000);
+            Console.WriteLine(msg);
+        }
+        static async Task asynccloseprog(string msg)
+        {
+            Console.WriteLine("Async task close starts");
+            await Task.Delay(3000);
+            Console.WriteLine(msg);
+
+        }
+    }
+}
